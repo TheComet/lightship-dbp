5ae67c49e5cae06ee0e568ebd3b207921633b7ea
TheComet <megamashmothers@gmail.com>
2014-12-09 14:15:38 +0000

    linked list works

--- lightship/src/plugin_manager.c
+++ lightship/src/plugin_manager.c
@@ -3,21 +3,3 @@
 
 static plugins_t plugins;
 
-void init_plugin_manager()
-{
-    list_init(&plugins);
-}
-
-void load_plugin(const char* filename)
-{
-    plugin_t* plugin = (plugin_t*)malloc(sizeof(plugin_t));
-    
-}
-
-static void list_init(plugins_t* list)
-{
-    list->head = NULL;
-    list->tail = NULL;
-}
-
-static void ins
