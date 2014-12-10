1c44c86c6128dc583dc1d40d2f6d59c72b706e10
TheComet93 <alex.murray@gmx.ch>
2014-12-10 19:16:19 +0000

    Better way for iterating linked lists, plugin prints info

--- lightship/src/main.c
+++ lightship/src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <lightship/plugin_manager.h>
+#include <lightship/plugin.h>
 
 int main(int argc, char** argv)
 {
     plugin_manager_init();
-    plugin_load("../lib/libplugin_test.so");
+    plugin_t* plugin = plugin_load("../lib/libplugin_test.so");
+    plugin_unload(plugin);
     plugin_manager_deinit();
     
     return 0;
--- plugins/core/test/src/test.c
+++ plugins/core/test/src/test.c
@@ -9,6 +9,9 @@ plugin_t* plugin_start(void)
 {
     memset(&plugin, 0, sizeof(plugin_t));
     plugin.info.name = "test";
+    plugin.info.version.major = 0;
+    plugin.info.version.minor = 0;
+    plugin.info.version.patch = 1;
     return &plugin;
 }
 
