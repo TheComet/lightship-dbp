ef10e7725bdaac3702c45edc6675d875a7afc33e
TheComet93 <megamashmothers@gmail.com>
2014-12-13 00:50:06 +0100

    Abstracting directory scans

---
+++ util/src/platform/linux/dir_linux.c
@@ -0,0 +1,41 @@
+#define _SVID_SOURCE
+
+#include <util/config.h>
+
+#ifdef LIGHTSHIP_PLATFORM_LINUX
+
+#include <dirent.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <util/linked_list.h>
+#include <util/string.h>
+
+void get_directory_listing(struct list_t* list, const char* dir)
+{
+    DIR* fd;
+    struct dirent* dp;
+
+    /* open directory */
+    fd = opendir(dir);
+    if(!fd)
+    {
+        fprintf_strings(stderr, 3, "Error searching directory \"", dir, "\": ");
+        perror("");
+        return;
+    }
+
+    /* copy contents of directory into linked list */
+    do
+    {
+        errno = 0;
+        list_push(list, malloc_string(dp->d_name));
+    } while ((dp = readdir(fd)) != NULL);
+
+    /* catch any errors */
+    if(errno != 0)
+        perror("Error reading directory");
+
+    closedir(fd);
+}
+
+#endif /* #ifdef LIGHTSHIP_PLATFORM_LINUX */
