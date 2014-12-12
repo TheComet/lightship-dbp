5a7cf6350dbd56fd700770bfd32c2a3bf44ac7d2
TheComet <megamashmothers@gmail.com>
2014-12-12 20:22:44 +0100

    Making everything C89 compliant so Microsoft is happy

--- util/src/string.c
+++ util/src/string.c
@@ -4,36 +4,38 @@
 #include <stdlib.h>
 #include <util/string.h>
 
-inline static int safe_strlen(const char* str)
+__inline static int safe_strlen(const char* str)
 {
     if(str)
         return strlen(str);
     return 0;
 }
 
-inline static void safe_strcat(char* target, const char* source)
+__inline static void safe_strcat(char* target, const char* source)
 {
     if(source)
         strcat(target, source);
 }
 
-void fprintf_strings(FILE* file, int num_strs, ...)
+void fprintf_strings(struct FILE* file, int num_strs, ...)
 {
+	int total_length = 0;
+	int i;
+	char* buffer;
     /* compute total length of all strings combined and allocate a buffer able
      * to contain all strings plus a null terminator */
     va_list ap;
     va_start(ap, num_strs);
-    int total_length = 0;
-    for(int i = 0; i != num_strs; ++i)
+    for(i = 0; i != num_strs; ++i)
         total_length += safe_strlen(va_arg(ap, char*));
     total_length += sizeof(char*); /* null terminator */
-    char* buffer = malloc(total_length * sizeof(char*));
+    buffer = (char*)malloc(total_length * sizeof(char*));
     va_end(ap);
     
     /* concatinate all strings into the allocated buffer */
     va_start(ap, num_strs);
     strcpy(buffer, va_arg(ap, char*));
-    for(int i = 1; i < num_strs; ++i)
+    for(i = 1; i < num_strs; ++i)
         safe_strcat(buffer, va_arg(ap, char*));
     va_end(ap);
     
