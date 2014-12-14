b2131c01d2b2b37ee87818ceae558d991bf0c74e
TheComet93 <megamashmothers@gmail.com>
2014-12-14 04:18:13 +0100

    Macro for iterating a vector's elements. Refined documentation in all files

--- util/include/util/string.h
+++ util/include/util/string.h
@@ -2,17 +2,17 @@
 
 /*!
  * @brief Concatenates n number of strings and prints it to the specified file.
- * @param file The file to print to.
- * @param num_strs The number of strings that should be concatenated.
- * @param ...str A list of comma-seperated strings to concatenate.
+ * @param [in] file The file to print to.
+ * @param [in] num_strs The number of strings that should be concatenated.
+ * @param [in] ...str A list of comma-seperated strings to concatenate.
  */
 void fprintf_strings(FILE* file, int num_strs, ...);
 
 /*!
  * @brief Concatenates n number of strings and returns the buffer.
  * @note The buffer needs to be freed manually when it is no longer required.
- * @param num_strs The number of strings that should be concatenated.
- * @param ...str A list of comma-seperated strings to concatenate.
+ * @param [in] num_strs The number of strings that should be concatenated.
+ * @param [in] ...str A list of comma-seperated strings to concatenate.
  * @return The new buffer containing all concatenated strings.
  */
 char* cat_strings(int num_strs, ...);
@@ -20,7 +20,7 @@ char* cat_strings(int num_strs, ...);
 /*!
  * @brief Copies the specified into a new buffer and returns it.
  * @note The buffer needs to be freed manually when it is no longer required.
- * @param str The string to copy.
+ * @param [in] str The string to copy.
  * @return The new buffer containing a copy of the input string.
  */
 char* malloc_string(const char* str);
