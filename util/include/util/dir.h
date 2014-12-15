bf12795e19aea383303e560bf17f395bee3fc973
TheComet <megamashmothers@gmail.com>
2014-12-15 22:32:07 +0100

    Warning about malloc() in directory listing

--- util/include/util/dir.h
+++ util/include/util/dir.h
@@ -4,6 +4,8 @@ struct list_t;
  * @brief Scans a specified directory and saves the listings in the specified
  * list.
  * @note If the list is not empty, then the data is simply appended.
+ * @note The string entries are allocated with malloc() and must be freed
+ * manually when erasing the list.
  * @param [in] list The list to store the directory listings in.
  * @param [in] dir The directory to scan (relative or absolute).
  */
