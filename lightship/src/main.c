f059d06b2eb40ad70531a9c1028500b06310332a
TheComet93 <alex.murray@gmx.ch>
2014-12-20 00:30:57 +0100

    first version of service functions

--- lightship/src/main.c
+++ lightship/src/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include "lightship/plugin_manager.h"
+#include "lightship/services.h"
 #include "util/plugin.h"
 #include "util/vector.h"
 
+struct plugin_t* plugin_main_loop = NULL;
 struct plugin_t* plugin_renderer = NULL;
 struct plugin_t* plugin_input = NULL;
 
 void load_core_plugins(void)
 {
     struct plugin_info_t target;
+    
+    /* load main loop plugin */
+    target.name = "main_loop";
+    target.version.major = 0;
+    target.version.minor = 0;
+    target.version.patch = 1;
+    plugin_main_loop = plugin_load(&target, PLUGIN_VERSION_MINIMUM);
     
     /* load graphics plugin */
     target.name = "renderer_gl";
@@ -27,6 +36,8 @@ void load_core_plugins(void)
 
 void start_core_plugins(void)
 {
+    if(plugin_main_loop)
+        plugin_start(plugin_main_loop);
     if(plugin_renderer)
         plugin_start(plugin_renderer);
     if(plugin_input)
@@ -36,6 +47,7 @@ void start_core_plugins(void)
 int main(int argc, char** argv)
 {
     plugin_manager_init();
+    services_init();
     load_core_plugins();
     start_core_plugins();
 
