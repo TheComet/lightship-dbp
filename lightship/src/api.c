3d0b5cd333de7c685ce1fae556b2132c3204c38f
TheComet93 <megamashmothers@gmail.com>
2014-12-20 01:46:48 +0100

    Plugins can now register and request services. Lightship passes a struct of host functions to each plugin upon initialisation

---
+++ lightship/src/api.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "lightship/api.h"
+#include "lightship/plugin_manager.h"
+#include "lightship/services.h"
+
+struct lightship_api_t g_api;
+
+void api_init(void)
+{
+    memset(&g_api, 0, sizeof(struct lightship_api_t));
+    
+    g_api.plugin_load = plugin_load;
+    g_api.plugin_unload = plugin_unload;
+    g_api.plugin_get_by_name = plugin_get_by_name;
+    g_api.service_register = service_register;
+    g_api.service_unregister = service_unregister;
+    g_api.service_get = service_get;
+}
