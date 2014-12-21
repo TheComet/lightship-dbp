034e7f56aca42b98846ef2aad8c401f510fa556c
TheComet93 <megamashmothers@gmail.com>
2014-12-21 02:55:26 +0100

    Events seem to work, but it needs a lot of work. Currently ugly as fuck

--- lightship/src/api.c
+++ lightship/src/api.c
@@ -18,5 +18,10 @@ void api_init(void)
     g_api.service_get = service_get;
     g_api.event_create = event_create;
     g_api.event_destroy = event_destroy;
+    g_api.event_destroy_plugin_event = event_destroy_plugin_event;
+    g_api.event_destroy_all_plugin_events = event_destroy_all_plugin_events;
     g_api.event_get = event_get;
+    g_api.event_register_listener = event_register_listener;
+    g_api.event_unregister_listener = event_unregister_listener;
+    g_api.event_unregister_all_listeners = event_unregister_all_listeners;
 }
