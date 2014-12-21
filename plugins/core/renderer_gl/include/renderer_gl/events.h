adc9a0647ecc4320318e1fc2bf5acfcef9d9a9ae
TheComet93 <alex.murray@gmx.ch>
2014-12-21 23:51:32 +0100

    Documentation for events

--- plugins/core/renderer_gl/include/renderer_gl/events.h
+++ plugins/core/renderer_gl/include/renderer_gl/events.h
@@ -4,7 +4,7 @@ struct plugin_t;
 struct lightship_api_t;
 
 /* events this plugin defines */
-EVENT(evt_close_window);
+EVENT_H(evt_close_window)
 
 void register_events(struct plugin_t* plugin, struct lightship_api_t* api);
 void register_event_listeners(struct plugin_t* plugin, struct lightship_api_t* api);
