f8733a90ff1c28f558253c02fcf3c3247089675c
TheComet93 <megamashmothers@gmail.com>
2014-12-23 23:41:13 +0100

    Getting backtraces on osx in memory reports now

--- util/src/platform/osx/backtrace_osx.c
+++ util/src/platform/osx/backtrace_osx.c
@@ -1,6 +1,13 @@
 #include "util/backtrace.h"
+#include <execinfo.h>
 
 char** get_backtrace(intptr_t* size )
 {
-    /* TODO */
+    void* array[BACKTRACE_SIZE];
+    char** strings;
+    
+    *size = backtrace(array, BACKTRACE_SIZE);
+    strings = backtrace_symbols(array, *size);
+    
+    return strings;
 }
