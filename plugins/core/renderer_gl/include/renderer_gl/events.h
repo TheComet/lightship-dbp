5e711a75a3b06e622d90ad12dc9f1a29a4f3f9ba
TheComet93 <alex.murray@gmx.ch>
2014-12-25 16:00:29 +0000

    Mouse scroll event added

--- plugins/core/renderer_gl/include/renderer_gl/events.h
+++ plugins/core/renderer_gl/include/renderer_gl/events.h
@@ -9,6 +9,7 @@ EVENT_H(evt_key_release)
 EVENT_H(evt_mouse_move)
 EVENT_H(evt_mouse_button_press)
 EVENT_H(evt_mouse_button_release)
+EVENT_H(evt_mouse_scroll)
 
 void register_events(struct plugin_t* plugin);
 void register_event_listeners(struct plugin_t* plugin);
