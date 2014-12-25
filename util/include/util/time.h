1e8b301be1281a05d435a7c7ec2faf594ba4f1a2
TheComet93 <megamashmothers@gmail.com>
2014-12-25 13:36:27 +0000

    Changed time types to uint64_t

--- util/include/util/time.h
+++ util/include/util/time.h
@@ -2,7 +2,8 @@
 #define LIGHTSHIP_UTIL_TIME_H
 
 #include "util/config.h"
+#include "util/pstdint.h"
 
-LIGHTSHIP_PUBLIC_API long get_time_in_microseconds(void);
+LIGHTSHIP_PUBLIC_API uint64_t get_time_in_microseconds(void);
 
 #endif /* LIGHTSHIP_UTIL_TIME_H */
