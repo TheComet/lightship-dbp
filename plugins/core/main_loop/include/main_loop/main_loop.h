749bc0a6107b6967115028cd025e176ffe596791
TheComet <megamashmothers@gmail.com>
2014-12-25 04:19:45 +0000

    Update loop and render loop implemented in main loop plugin

--- plugins/core/main_loop/include/main_loop/main_loop.h
+++ plugins/core/main_loop/include/main_loop/main_loop.h
@@ -1,4 +1,28 @@
 struct event_t;
 
+struct main_loop_statistics_t
+{
+    long last_update;
+    long update_counter_rel;
+    long render_counter_rel;
+    unsigned int update_frame_rate;
+    unsigned int render_frame_rate;
+};
+
+struct main_loop_t
+{
+    char is_looping;
+    unsigned int fps;
+    long time_begin; /* NOTE all times are in microseconds */
+    long time_between_frames;
+    long update_loop_counter;
+    struct main_loop_statistics_t statistics;
+};
+
 void main_loop_start(void);
 void main_loop_stop(struct event_t* evt, void* args);
+void main_loop_reset_timer(void);
+long main_loop_get_elapsed_time(void);
+
+#include "util/event_api.h"
+EVENT_LISTENER(on_stats, struct main_loop_statistics_t* stats);
--- plugins/core/main_loop/src/events.c
+++ plugins/core/main_loop/src/events.c
@@ -2,13 +2,18 @@
 #include "main_loop/main_loop.h"
 
 EVENT_C(evt_render)
+EVENT_C(evt_update)
+EVENT_C(evt_stats)
 
 void register_events(struct plugin_t* plugin)
 {
     evt_render = event_create(plugin, "render");
+    evt_update = event_create(plugin, "update");
+    evt_stats = event_create(plugin, "stats");
 }
 
 void register_event_listeners(struct plugin_t* plugin)
 {
     event_register_listener(plugin, "renderer_gl.close_window", main_loop_stop);
+    event_register_listener(plugin, "main_loop.stats", on_stats);
 }
