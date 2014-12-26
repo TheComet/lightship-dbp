7951dc4f119822445bcf0970f66f0daeb3875cf0
TheComet93 <alex.murray@gmx.ch>
2014-12-26 15:35:35 +0000

    Converted libyaml to build with CMake build system, integrated into new yaml plugin

---
+++ plugins/core/yaml/include/yaml/services.h
@@ -0,0 +1,3 @@
+struct plugin_t;
+
+void register_services(struct plugin_t* plugin);
---
+++ plugins/core/yaml/src/services.c
@@ -0,0 +1,15 @@
+#include "lightship/api.h"
+#include "yaml/services.h"
+
+void register_services(struct plugin_t* plugin)
+{
+    /* -----------------------------------------------------
+    * All services this plugin supports
+    * ---------------------------------------------------*/
+    /*
+     * service_register(plugin, "service_name_1", service_callback_1);
+     * service_register(plugin, "service_name_2", service_callback_2);
+     * etc...
+     */
+
+}
