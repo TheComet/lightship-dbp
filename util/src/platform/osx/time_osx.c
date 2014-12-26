e27340e790a14152f08893ea216e00c25c7630ba
TheComet <megamashmothers@gmail.com>
2014-12-26 18:20:31 +0000

    Project now cross compiles successfully

---
+++ util/src/platform/osx/time_osx.c
@@ -0,0 +1,10 @@
+#include "util/time.h"
+#include <sys/time.h>
+#include <stdlib.h>
+
+int64_t get_time_in_microseconds(void)
+{
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    return time.tv_usec + time.tv_sec*1000000;
+}
