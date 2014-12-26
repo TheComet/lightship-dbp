d4cd7d741abb8e957284ef2a41388190b0bb932f
TheComet93 <megamashmothers@gmail.com>
2014-12-26 15:35:35 +0000

    Converted libyaml to build with CMake build system, integrated into new yaml plugin

---
+++ plugins/core/yaml/src/plugin_yaml.c
@@ -0,0 +1,51 @@
+#include "yaml/config.h"   /* configurations for this plugin */
+#include "lightship/api.h"   /* lightship API so we can register and call services */
+#include "util/config.h"     /* platform macros and definitions */
+#include "util/plugin.h"     /* plugin api */
+#include "yaml/services.h" /* plugin services */
+#include "yaml/events.h"   /* plugin events */
+
+struct plugin_t* g_plugin = NULL;
+
+void create_and_init_plugin(void)
+{
+    /* create plugin object - host requires this */
+    g_plugin = plugin_create();
+    
+    /* set plugin information - Change this in the file "CMakeLists.txt" */
+    plugin_set_info(g_plugin,
+            YAML_NAME,         /* name */
+            YAML_CATEGORY,     /* category */
+            YAML_AUTHOR,       /* author */
+            YAML_DESCRIPTION,  /* description */
+            YAML_WEBSITE       /* website */
+    );
+    plugin_set_programming_language(g_plugin,
+            PLUGIN_PROGRAMMING_LANGUAGE_C
+    );
+    plugin_set_version(g_plugin,
+            YAML_VERSION_MAJOR,
+            YAML_VERSION_MINOR,
+            YAML_VERSION_PATCH
+    );
+}
+
+PLUGIN_INIT()
+{
+    create_and_init_plugin();
+    register_services(g_plugin);
+    register_events(g_plugin);
+    return g_plugin;
+}
+
+PLUGIN_START()
+{
+    register_event_listeners(g_plugin);
+
+    return PLUGIN_SUCCESS;
+}
+
+PLUGIN_STOP()
+{
+    plugin_destroy(g_plugin);
+}
