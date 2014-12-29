0e6b4b8066cdec160152294f21691cc02d2184ba
TheComet93 <megamashmothers@gmail.com>
2014-12-29 19:51:42 +0000

    YAML parser successfully loads files into internal ptree

--- util/include/util/ptree.h
+++ util/include/util/ptree.h
@@ -5,23 +5,29 @@ struct ptree_t
 {
     struct unordered_vector_t children;
 #ifdef _DEBUG
-    char* name;
+    char* key;
 #endif
     uint32_t hash;
-    void* data;
+    void* value;
 };
 
 LIGHTSHIP_PUBLIC_API struct ptree_t*
-ptree_create(const char* name, void* data);
+ptree_create(const char* key, void* data);
 
 LIGHTSHIP_PUBLIC_API void
-ptree_init_ptree(struct ptree_t* tree, const char* name, void* data);
+ptree_init_ptree(struct ptree_t* tree, const char* key, void* data);
 
 LIGHTSHIP_PUBLIC_API void
 ptree_destroy(struct ptree_t* tree);
 
-LIGHTSHIP_PUBLIC_API void
-ptree_add_node(struct ptree_t* tree, const char* name, void* data);
+LIGHTSHIP_PUBLIC_API struct ptree_t*
+ptree_add_node(struct ptree_t* tree, const char* key, void* data);
+
+LIGHTSHIP_PUBLIC_API void*
+ptree_find_local_by_key(const struct ptree_t* tree, const char* key);
+
+LIGHTSHIP_PUBLIC_API void*
+ptree_find_by_key(const struct ptree_t* tree, const char* key);
 
 LIGHTSHIP_PUBLIC_API void
 ptree_print(struct ptree_t* tree);
