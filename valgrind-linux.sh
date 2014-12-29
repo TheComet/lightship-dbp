45fed246055abd302706878adec9188f667d7591
TheComet93 <alex.murray@gmx.ch>
2014-12-29 19:52:37 +0000

    script for running valgrind on linux

---
+++ valgrind-linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd build/bin
+valgrind --num-callers=50 --leak-resolution=high --leak-check=full --track-origins=yes --show-reachable=yes --show-possibly-lost=yes  --time-stamp=yes ./lightship
+cd .. && cd ..
