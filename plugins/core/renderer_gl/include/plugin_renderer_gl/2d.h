6d4a69c54635249b8e91043ad729482613712f0f
TheComet93 <megamashmothers@gmail.com>
2014-12-31 03:15:43 +0000

    First working api for drawing lines

--- plugins/core/renderer_gl/include/plugin_renderer_gl/2d.h
+++ plugins/core/renderer_gl/include/plugin_renderer_gl/2d.h
@@ -1,3 +1,23 @@
+#include "GL/glew.h"
+#include "util/unordered_vector.h"
+
+
+/* vertex type for 2d objects */
+struct vertex_2d_t
+{
+    GLfloat position[2];
+    GLfloat diffuse[4];
+};
+
+struct batch_t
+{
+    GLuint vbo;
+    struct unordered_vector_t vertex_data;
+};
+
 void init_2d(void);
-void line(float x1, float y1, float x2, float y2);
+void deinit_2d(void);
+void batch_2d_begin(void);
+void batch_2d_end(void);
+void line(float x1, float y1, float x2, float y2, uint32_t colour1, uint32_t colour2);
 void draw_2d(void);
--- plugins/core/renderer_gl/src/2d.c
+++ plugins/core/renderer_gl/src/2d.c
@@ -4,82 +4,102 @@
 #include "GL/glew.h"
 #include "glfw3.h"
 
-/* vertex type for 2d objects */
-struct vertex_2d_t
-{
-    GLfloat position[2];
-    GLfloat diffuse[4];
-};
-
-struct line_2d_t
-{
-    GLuint vbo;
-    GLuint vio;
-    GLuint 
-};
-
-/* vao for 2d objects */
-GLuint vao_2d;
-
-/* define line */
-#define LINE_2D_NUM_VERTICES 2
-#define LINE_2D_NUM_INDICES 2
-GLuint line_2d_vbo;
-GLuint line_2d_vio;
-GLuint line_2d_shader_id;
-GLuint line_2d_colour_uniform_location;
-GLfloat line_2d_colour[3];
-static struct vertex_2d_t line_2d_vertex_data[LINE_2D_NUM_VERTICES] = {
-    {{-1.0, -1.0}, {1.0, 0.0, 0.0, 1.0}},
-    {{ 1.0,  1.0}, {0.0, 1.0, 0.0, 1.0}}
-};
-static GLubyte line_2d_index_data[LINE_2D_NUM_INDICES] = {
-    0,
-    1
-};
+GLuint g_vao_2d; /* vao for 2d objects */
+GLuint g_line_shader_id;
+struct batch_t* g_current_batch = NULL;
+struct unordered_vector_t g_batches;
 
 void init_2d(void)
 {
     /* setup VAO for 2d draw calls */
-    glGenVertexArrays(1, &vao_2d);
-    glBindVertexArray(vao_2d);
+    glGenVertexArrays(1, &g_vao_2d);
     
-        /* setup vertex and index buffers for 2D lines */
-        glGenBuffers(1, &line_2d_vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, line_2d_vbo);
-        glBufferData(GL_ARRAY_BUFFER, LINE_2D_NUM_VERTICES*sizeof(struct vertex_2d_t), line_2d_vertex_data, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0,                      /* attribute 0 */
-                              2,                      /* size, position[2] */
-                              GL_FLOAT,               /* type */
-                              GL_FALSE,               /* normalise? */
-                              sizeof(struct vertex_2d_t),
-                              (void*)offsetof(struct vertex_2d_t, position));
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1,                      /* attribute 1 */
-                              4,                      /* diffuse[4] */
-                              GL_FLOAT,               /* type */
-                              GL_FALSE,               /* normalise? */
-                              sizeof(struct vertex_2d_t),
-                              (void*)offsetof(struct vertex_2d_t, diffuse));
-        glGenBuffers(1, &line_2d_vio);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, line_2d_vio);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, LINE_2D_NUM_INDICES*sizeof(GLubyte), line_2d_index_data, GL_STATIC_DRAW);
-        line_2d_shader_id = load_shader("fx/line_2d");
-        glUseProgram(line_2d_shader_id);
-
-    /* done, unbind vao */
-    glBindVertexArray(0);
+    /* load shaders */
+    g_line_shader_id = load_shader("fx/line_2d");
+    
+    unordered_vector_init_vector(&g_batches, sizeof(struct batch_t));
 }
 
-void line(float x1, float y1, float x2, float y2)
+void deinit_2d(void)
 {
+    UNORDERED_VECTOR_FOR_EACH(&g_batches, struct batch_t, batch)
+    {
+        unordered_vector_clear_free(&batch->vertex_data);
+    }
+    unordered_vector_clear_free(&g_batches);
+}
+
+void batch_2d_begin(void)
+{
+    if(g_current_batch)
+        return;
+
+    g_current_batch = (struct batch_t*)unordered_vector_push_emplace(&g_batches);
+    unordered_vector_init_vector(&g_current_batch->vertex_data, sizeof(struct vertex_2d_t));
+}
+
+void line(float x1, float y1, float x2, float y2, uint32_t colour1, uint32_t colour2)
+{
+    struct vertex_2d_t* vertex;
+    if(!g_current_batch)
+        return;
     
+    /* add two new vertices to the batch */
+    vertex = (struct vertex_2d_t*)unordered_vector_push_emplace(&g_current_batch->vertex_data);
+    vertex->position[0] = x1;
+    vertex->position[1] = y1;
+    vertex->diffuse[0] = (float)((colour1 >> 24) & 0x000000FF) / 255.0;
+    vertex->diffuse[1] = (float)((colour1 >> 16) & 0x000000FF) / 255.0;
+    vertex->diffuse[2] = (float)((colour1 >>  8) & 0x000000FF) / 255.0;
+    vertex->diffuse[3] = (float)((colour1 >>  0) & 0x000000FF) / 255.0;
+    vertex = (struct vertex_2d_t*)unordered_vector_push_emplace(&g_current_batch->vertex_data);
+    vertex->position[0] = x2;
+    vertex->position[1] = y2;
+    vertex->diffuse[0] = (float)((colour2 >> 24) & 0x000000FF) / 255.0;
+    vertex->diffuse[1] = (float)((colour2 >> 16) & 0x000000FF) / 255.0;
+    vertex->diffuse[2] = (float)((colour2 >>  8) & 0x000000FF) / 255.0;
+    vertex->diffuse[3] = (float)((colour2 >>  0) & 0x000000FF) / 255.0;
+}
+
+void batch_2d_end(void)
+{
+    if(!g_current_batch)
+        return;
+    
+    glBindVertexArray(g_vao_2d);
+        glGenBuffers(1, &g_current_batch->vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, g_current_batch->vbo);
+            glBufferData(GL_ARRAY_BUFFER,
+                         g_current_batch->vertex_data.count * sizeof(struct vertex_2d_t),
+                         g_current_batch->vertex_data.data,
+                         GL_STATIC_DRAW);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0,                      /* attribute 0 */
+                                  2,                      /* size, position[2] */
+                                  GL_FLOAT,               /* type */
+                                  GL_FALSE,               /* normalise? */
+                                  sizeof(struct vertex_2d_t),
+                                  (void*)offsetof(struct vertex_2d_t, position));
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1,                      /* attribute 1 */
+                                  4,                      /* diffuse[4] */
+                                  GL_FLOAT,               /* type */
+                                  GL_FALSE,               /* normalise? */
+                                  sizeof(struct vertex_2d_t),
+                                  (void*)offsetof(struct vertex_2d_t, diffuse));
+        glUseProgram(g_line_shader_id);
+    
+    g_current_batch = NULL;
 }
 
 void draw_2d(void)
 {
-    glBindVertexArray(vao_2d);
-        glDrawElements(GL_LINES, LINE_2D_NUM_INDICES, GL_UNSIGNED_BYTE, NULL);
-    glBindVertexArray(0);
+    glBindVertexArray(g_vao_2d);
+    {
+        UNORDERED_VECTOR_FOR_EACH(&g_batches, struct batch_t, batch)
+        {
+            glBindBuffer(GL_ARRAY_BUFFER, batch->vbo);
+                glDrawArrays(GL_LINES, 0, batch->vertex_data.count);
+        }
+    }
 }
