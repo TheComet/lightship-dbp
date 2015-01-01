d7956731297ea4cd34cdd38d22c07d2b49f998ad
TheComet93 <alex.murray@gmx.ch>
2015-01-01 15:43:59 +0000

    Fixed some warnings, cross compilation works

--- cross-build/cross-gentoo-x86_64-pc-linux-gnu.sh
+++ cross-build/cross-gentoo-x86_64-pc-linux-gnu.sh
@@ -5,24 +5,28 @@
 
 # build everything
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCROSS_COMPILE_i486-pc-mingw32=ON -DCROSS_COMPILE_x86_64-pc-mingw32=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../dist/ ../.. || exit 1;
+cmake -DCMAKE_BUILD_TYPE=Release \
+	-DCROSS_COMPILE_i486-pc-mingw32=ON \
+	-Di486-pc-mingw32_INSTALL_PREFIX=$(pwd)/../dist/i486-pc-mingw32 \
+	-DCROSS_COMPILE_x86_64-pc-mingw32=ON \
+	-Dx86_64-pc-mingw32_INSTALL_PREFIX=$(pwd)/../dist/x86_64-pc-mingw32 \
+	-DCMAKE_INSTALL_PREFIX=$(pwd)/../dist/x86_64-pc-linux-gnu \
+	../.. || exit 1;
 make -j5 || exit 1;
 make install || exit 1;
 cd ..
 
-# correct output directory for host build
-mkdir dist/x86_64-pc-linux-gnu || exit 1;
-mv dist/bin dist/x86_64-pc-linux-gnu || exit 1;
-
 # generate run scripts for each platform
-echo -e "@ECHO OFF\nlightship.exe\nPAUSE" > dist/x86_64-pc-mingw32/bin/run.bat || exit 1;
-echo -e "@ECHO OFF\nlightship.exe\nPAUSE" > dist/i486-pc-mingw32/bin/run.bat || exit 1;
-echo -e "export LD_LIBRARY_PATH=.\n./lightship" > dist/x86_64-pc-linux-gnu/bin/run.sh || exit 1;
+echo -e "@ECHO OFF\ncd bin\nlightship.exe\nPAUSE\ncd .." > dist/x86_64-pc-mingw32/run.bat || exit 1;
+echo -e "@ECHO OFF\ncd bin\nlightship.exe\nPAUSE\ncd .." > dist/i486-pc-mingw32/run.bat || exit 1;
+echo -e "export LD_LIBRARY_PATH=\$(pwd)/lib\ncd bin\n./lightship\ncd .." > dist/x86_64-pc-linux-gnu/run.sh || exit 1;
 
 # compress
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on dist/archive/lightship-x86_64-pc-mingw32.7z dist/x86_64-pc-mingw32 || exit 1;
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on dist/archive/lightship-i486-pc-mingw32.7z dist/i486-pc-mingw32 || exit 1;
 tar --xz -cf dist/archive/lightship-x86_64-pc-linux-gnu.tar.xz dist/x86_64-pc-linux-gnu || exit 1;
 
+echo "================================================"
 echo "Done!"
+echo "================================================"
 
