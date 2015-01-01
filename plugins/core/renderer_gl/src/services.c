77b991b24c1911db1cb3c77457c6ea2d06930056
TheComet93 <alex.murray@gmx.ch>
2015-01-01 00:39:15 +0000

    Registering 2D drawing functions as services, also some formatting

--- plugins/core/renderer_gl/src/services.c
+++ plugins/core/renderer_gl/src/services.c
@@ -1,7 +1,11 @@
 #include "plugin_renderer_gl/services.h"
+#include "plugin_renderer_gl/2d.h"
 #include "util/services.h"
 
 void
 register_services(const struct plugin_t* plugin)
 {
+    service_register(plugin, "shapes_2d_begin", (intptr_t)shapes_2d_begin);
+    service_register(plugin, "shapes_2d_end", (intptr_t)shapes_2d_end);
+    service_register(plugin, "line_2d", (intptr_t)line_2d);
 }
