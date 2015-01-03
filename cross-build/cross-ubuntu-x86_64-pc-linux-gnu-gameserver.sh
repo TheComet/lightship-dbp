84d225ada03cb2e8a1399d02dd3021eef4579e43
TheComet93 <alex.murray@gmx.ch>
2015-01-03 21:34:06 +0100

    move archives to distribution folder

--- cross-build/cross-ubuntu-x86_64-pc-linux-gnu-gameserver.sh
+++ cross-build/cross-ubuntu-x86_64-pc-linux-gnu-gameserver.sh
@@ -20,10 +20,15 @@ echo -e "@ECHO OFF\ncd bin\nlightship.exe\nPAUSE\ncd .." > dist/x86_64-pc-mingw3
 echo -e "export LD_LIBRARY_PATH=\$(pwd)/lib\ncd bin\n./lightship\ncd .." > dist/x86_64-pc-linux-gnu/run.sh || exit 1;
 
 # compress
-7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on dist/archive/lightship-$1-$2-x86_64-pc-mingw32.7z dist/x86_64-pc-mingw32 || exit 1;
-tar --xz -cf dist/archive/lightship-$1-$2-x86_64-pc-linux-gnu.tar.xz dist/x86_64-pc-linux-gnu || exit 1;
+LINUX64=lightship-$1-$2-x86_64-pc-linux-gnu.tar.xz
+WIN64=lightship-$1-$2-x86_64-pc-mingw32.7z
+7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on dist/archive/$WIN64 dist/x86_64-pc-mingw32 || exit 1;
+tar --xz -cf dist/archive/$LINUX64 dist/x86_64-pc-linux-gnu || exit 1;
 
-# move to 
+# move to distribution folder
+echo "moving archives to /www/game/lightship/"
+mv dist/archive/$WIN64 /www/game/lightship/
+mv dist/archive/$LINUX64 /www/game/lightship/
 
 echo "================================================"
 echo "Done!"
