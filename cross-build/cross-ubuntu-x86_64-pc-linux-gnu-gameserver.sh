408ce333d6f7e7fbb42bc5c6203738003b5924ed
TheComet93 <alex.murray@gmx.ch>
2015-01-03 23:01:26 +0000

    Updating cross build scripts to be more friendly to edit in the future

--- cross-build/cross-ubuntu-x86_64-pc-linux-gnu-gameserver.sh
+++ cross-build/cross-ubuntu-x86_64-pc-linux-gnu-gameserver.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # clean build and dist tree from last build
 ./clean.sh
+
+WIN64=lightship-$1-$2-x86_64-pc-mingw32
+LINUX64=lightship-$1-$2-x86_64-pc-linux-gnu
 
 # build everything
 mkdir build && cd build
@@ -9,27 +12,25 @@ cmake -DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_HOST="game.pixelcloud.ch" \
 	-DMAKE_ARGS=-j9 \
 	-DCROSS_COMPILE_x86_64-pc-mingw32=ON \
-	-Dx86_64-pc-mingw32_INSTALL_PREFIX=$(pwd)/../dist/x86_64-pc-mingw32 \
-	-DCMAKE_INSTALL_PREFIX=$(pwd)/../dist/x86_64-pc-linux-gnu \
+	-Dx86_64-pc-mingw32_INSTALL_PREFIX=$(pwd)/../dist/$WIN64 \
+	-DCMAKE_INSTALL_PREFIX=$(pwd)/../dist/$LINUX64 \
 	../.. || exit 1;
 make -j9 || exit 1;
 make install || exit 1;
 cd ..
 
 # generate run scripts for each platform
-echo -e "@ECHO OFF\ncd bin\nlightship.exe\nPAUSE\ncd .." > dist/x86_64-pc-mingw32/run.bat || exit 1;
-echo -e "export LD_LIBRARY_PATH=\$(pwd)/lib\ncd bin\n./lightship\ncd .." > dist/x86_64-pc-linux-gnu/run.sh || exit 1;
+echo -e "@ECHO OFF\ncd bin\nlightship.exe\nPAUSE\ncd .." > dist/$WIN64/run.bat || exit 1;
+echo -e "export LD_LIBRARY_PATH=\$(pwd)/lib\ncd bin\n./lightship\ncd .." > dist/$LINUX64/run.sh || exit 1;
 
 # compress
-LINUX64=lightship-$1-$2-x86_64-pc-linux-gnu.tar.xz
-WIN64=lightship-$1-$2-x86_64-pc-mingw32.7z
-7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on dist/archive/$WIN64 dist/x86_64-pc-mingw32 || exit 1;
-tar --xz -cf dist/archive/$LINUX64 dist/x86_64-pc-linux-gnu || exit 1;
+7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on dist/archive/$WIN64\.7z ./dist/$WIN64/ || exit 1;
+tar --xz -cvf dist/archive/$LINUX64\.tar.xz -C dist/ $LINUX64/ || exit 1;
 
 # move to distribution folder
 echo "moving archives to /www/game/lightship/"
-mv dist/archive/$WIN64 /www/game/lightship/
-mv dist/archive/$LINUX64 /www/game/lightship/
+mv dist/archive/$WIN64\.7z /www/game/lightship/
+mv dist/archive/$LINUX64\.tar.xz /www/game/lightship/
 
 echo "================================================"
 echo "Done!"
