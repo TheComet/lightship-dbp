a477666aa606fe8a4c0f7ae6554be60d2378e81a
TheComet93 <megamashmothers@gmail.com>
2015-01-12 17:27:22 +0100

    Added map bug to unit tests

--- tests/src/test_map.cpp
+++ tests/src/test_map.cpp
@@ -75,11 +75,11 @@ TEST(NAME, insertion_random)
     struct map_t* map = map_create();
     
     int a=56, b=45, c=18, d=27, e=84;
-    map_insert(map,26, &a);
-    map_insert(map,44, &b);
-    map_insert(map,82, &c);
-    map_insert(map,41, &d);
-    map_insert(map,70, &e);
+    map_insert(map, 26, &a);
+    map_insert(map, 44, &b);
+    map_insert(map, 82, &c);
+    map_insert(map, 41, &d);
+    map_insert(map, 70, &e);
     
     ASSERT_EQ(a, *(int*)map_find(map, 26));
     ASSERT_EQ(d, *(int*)map_find(map, 41));
@@ -90,46 +90,180 @@ TEST(NAME, insertion_random)
     map_destroy(map);
 }
 
-TEST(NAME, erase_elements)
+TEST(NAME, erase_deletes_underlying_vector)
 {
     struct map_t* map = map_create();
     
+    int a=53;
+    map_insert(map, 0, &a);
+    map_insert(map, 1, &a);
+    map_insert(map, 2, &a);
+    
+    map_clear(map);
+    
+    ASSERT_EQ(0, map->vector.count);
+    ASSERT_EQ(NULL, map->vector.data);
+    
+    map_destroy(map);
+}
+
+TEST(NAME, count_returns_correct_number)
+{
+    struct map_t* map = map_create();
+    
+    int a=53;
+    map_insert(map, 0, &a);
+    map_insert(map, 1, &a);
+    map_insert(map, 2, &a);
+    
+    ASSERT_EQ(3, map_count(map));
+    
+    map_destroy(map);
+}
+
+TEST(NAME, erase_elements)
+{
+    struct map_t* map = map_create();
+
     int a=56, b=45, c=18, d=27, e=84;
     map_insert(map, 0, &a);
     map_insert(map, 1, &b);
     map_insert(map, 2, &c);
     map_insert(map, 3, &d);
     map_insert(map, 4, &e);
-    
+
     ASSERT_EQ(c, *(int*)map_erase(map, 2));
-    
+
     /* 4 */
     ASSERT_EQ(a, *(int*)map_find(map, 0));
     ASSERT_EQ(b, *(int*)map_find(map, 1));
     ASSERT_EQ(d, *(int*)map_find(map, 3));
     ASSERT_EQ(e, *(int*)map_find(map, 4));
-    
+
     ASSERT_EQ(e, *(int*)map_erase(map, 4));
-    
+
     /* 3 */
     ASSERT_EQ(a, *(int*)map_find(map, 0));
     ASSERT_EQ(b, *(int*)map_find(map, 1));
     ASSERT_EQ(d, *(int*)map_find(map, 3));
-    
+
     ASSERT_EQ(a, *(int*)map_erase(map, 0));
-    
+
     /* 2 */
     ASSERT_EQ(b, *(int*)map_find(map, 1));
     ASSERT_EQ(d, *(int*)map_find(map, 3));
-    
+
     ASSERT_EQ(b, *(int*)map_erase(map, 1));
-    
+
     /* 1 */
     ASSERT_EQ(d, *(int*)map_find(map, 3));
-    
+
     ASSERT_EQ(d, *(int*)map_erase(map, 3));
-    
+
     ASSERT_EQ(NULL, map_erase(map, 2));
+
+    map_destroy(map);
+}
+
+TEST(NAME, reinsertion_forwards)
+{
+    struct map_t* map = map_create();
+
+    int a=56, b=45, c=18, d=27, e=84;
+    map_insert(map, 0, &a);
+    map_insert(map, 1, &b);
+    map_insert(map, 2, &c);
+    map_insert(map, 3, &d);
+    map_insert(map, 4, &e);
+
+    map_erase(map, 4);
+    map_erase(map, 3);
+    map_erase(map, 2);
+
+    map_insert(map, 2, &c);
+    map_insert(map, 3, &d);
+    map_insert(map, 4, &e);
+
+    ASSERT_EQ(a, *(int*)map_find(map, 0));
+    ASSERT_EQ(b, *(int*)map_find(map, 1));
+    ASSERT_EQ(c, *(int*)map_find(map, 2));
+    ASSERT_EQ(d, *(int*)map_find(map, 3));
+    ASSERT_EQ(e, *(int*)map_find(map, 4));
+
+    map_destroy(map);
+}
+
+TEST(NAME, reinsertion_backwards)
+{
+    struct map_t* map = map_create();
+
+    int a=56, b=45, c=18, d=27, e=84;
+    map_insert(map, 4, &a);
+    map_insert(map, 3, &b);
+    map_insert(map, 2, &c);
+    map_insert(map, 1, &d);
+    map_insert(map, 0, &e);
+
+    map_erase(map, 0);
+    map_erase(map, 1);
+    map_erase(map, 2);
+
+    map_insert(map, 2, &c);
+    map_insert(map, 1, &d);
+    map_insert(map, 0, &e);
+
+    ASSERT_EQ(e, *(int*)map_find(map, 0));
+    ASSERT_EQ(d, *(int*)map_find(map, 1));
+    ASSERT_EQ(c, *(int*)map_find(map, 2));
+    ASSERT_EQ(b, *(int*)map_find(map, 3));
+    ASSERT_EQ(a, *(int*)map_find(map, 4));
+
+    map_destroy(map);
+}
+
+TEST(NAME, reinsertion_random)
+{
+    struct map_t* map = map_create();
+
+    int a=56, b=45, c=18, d=27, e=84;
+    map_insert(map, 26, &a);
+    map_insert(map, 44, &b);
+    map_insert(map, 82, &c);
+    map_insert(map, 41, &d);
+    map_insert(map, 70, &e);
+
+    map_erase(map, 44);
+    map_erase(map, 70);
+    map_erase(map, 26);
+
+    map_insert(map, 26, &a);
+    map_insert(map, 70, &e);
+    map_insert(map, 44, &b);
+
+    ASSERT_EQ(a, *(int*)map_find(map, 26));
+    ASSERT_EQ(d, *(int*)map_find(map, 41));
+    ASSERT_EQ(b, *(int*)map_find(map, 44));
+    ASSERT_EQ(e, *(int*)map_find(map, 70));
+    ASSERT_EQ(c, *(int*)map_find(map, 82));
+
+    map_destroy(map);
+}
+
+TEST(NAME, inserting_duplicate_keys_doesnt_replace_existing_elements)
+{
+    struct map_t* map = map_create();
+    
+    int a=56, b=45, c=18;
+    map_insert(map, 5, &a);
+    map_insert(map, 3, &a);
+    
+    map_insert(map, 5, &b);
+    map_insert(map, 4, &b);
+    map_insert(map, 3, &c);
+    
+    ASSERT_EQ(a, *(int*)map_find(map, 3));
+    ASSERT_EQ(b, *(int*)map_find(map, 4));
+    ASSERT_EQ(a, *(int*)map_find(map, 5));
     
     map_destroy(map);
 }
