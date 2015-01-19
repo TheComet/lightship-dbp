b63eead3821b0e979b86b62f6a737015a22f9f21
TheComet93 <alex.murray@gmx.ch>
2015-01-19 18:11:21 +0100

    Added button click events

--- plugins/core/menu/include/plugin_menu/button.h
+++ plugins/core/menu/include/plugin_menu/button.h
@@ -1,4 +1,5 @@
 #include "util/pstdint.h"
+#include "util/event_api.h"
 
 #define BUTTON_COLOUR_NORMAL 0xFFFFFFFF
 
@@ -23,3 +24,16 @@ void button_deinit(void);
 uint32_t button_create(const char* text, float x, float y, float width, float height);
 void button_destroy(uint32_t ID);
 void button_destroy_all(void);
+
+/*!
+ * @brief Tests if the specified point is on top of the specified button.
+ * @param button The button object to test. If NULL is specified, all buttons
+ * are tested.
+ * @param x The x coordinate of the dot.
+ * @param y The y coordinate of the dot.
+ * @return Returns the button object being collided with, or NULL if there is
+ * no collision.
+ */
+struct button_t* button_collision(struct button_t* button, float x, float y);
+
+EVENT_LISTENER3(on_mouse_clicked, char mouse_btn, double x, double y);
