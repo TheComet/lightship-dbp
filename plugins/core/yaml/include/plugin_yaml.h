987a452582b37d0f421c60f6b1e3ba7afcf0c918
TheComet93 <alex.murray@gmx.ch>
2015-01-23 23:49:18 +0100

    New service functions for registering a service with type info

--- plugins/core/yaml/include/plugin_yaml.h
+++ plugins/core/yaml/include/plugin_yaml.h
@@ -1,5 +1,7 @@
 #include "util/pstdint.h"
 
+struct ptree_t;
+
 typedef uint32_t        (*yaml_load_func)           (const char*);
 typedef char*           (*yaml_get_value_func)      (uint32_t, const char*);
 typedef struct ptree_t* (*yaml_get_dom_func)        (uint32_t);
--- plugins/core/yaml/include/plugin_yaml/services.h
+++ plugins/core/yaml/include/plugin_yaml/services.h
@@ -1,12 +1,6 @@
 #include "util/pstdint.h"
 
 struct plugin_t;
-struct ptree_t;
-
-typedef uint32_t(*yaml_load_func)(const char*);
-typedef char*(*yaml_get_value_func)(uint32_t, const char*);
-typedef struct ptree_t*(*yaml_get_dom_func)(uint32_t);
-typedef void(*yaml_destroy_func)(uint32_t);
 
 void
 register_services(const struct plugin_t* plugin);
