3b2ba5c79b22b37a7a844311e00be503a1a7a862
TheComet93 <alex.murray@gmx.ch>
2015-02-01 02:45:42 +0100

    Fixed bug where literal strings were being copied into argument vector rather than pointers to strings

--- plugins/core/menu/src/services.c
+++ plugins/core/menu/src/services.c
@@ -21,12 +21,10 @@ struct service_t* text_load_characters;
 struct service_t* text_add_static_center_string;
 struct service_t* text_destroy_static_string;
 
-SERVICE(foo)
+SERVICE(foo_service)
 {
-    SERVICE_EXTRACT_ARGUMENT_PTR(0, t1, const char*);
-    SERVICE_EXTRACT_ARGUMENT_PTR(1, t2, const char*);
-    SERVICE_EXTRACT_ARGUMENT_PTR(2, t3, const char*);
-    printf("received strings: %s, %s, %s\n", t1, t2, t3);
+    SERVICE_EXTRACT_ARGUMENT_PTR(0, str, const char*);
+    printf("received string: %s!\n", str);
 }
 
 void
@@ -40,7 +38,8 @@ register_services(const struct plugin_t* plugin)
     SERVICE_REGISTER1(plugin, "button_destroy", button_destroy_wrapper, void, intptr_t);
     SERVICE_REGISTER1(plugin, "button_get_text", button_get_text_wrapper, wchar_t*, intptr_t);
     SERVICE_REGISTER1(plugin, "load", menu_load_wrapper, struct menu_t, const char*);
-    SERVICE_REGISTER3(plugin, "foo", foo, void, const char*, const char*, const char*);
+    
+    SERVICE_REGISTER1(plugin, "foo", foo_service, void, const char*);
 }
 
 char
