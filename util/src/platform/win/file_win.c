701a303d8d5a2d00e4d8952dc41f62d52facc77b
TheComet93 <alex.murray@gmx.ch>
2015-02-20 20:37:59 +0100

    Changing to GetFileSizeEx() so 32-bit builds are able to compile

--- util/src/platform/win/file_win.c
+++ util/src/platform/win/file_win.c
@@ -1,42 +1,42 @@
 #include "util/file.h"
 #include "util/log.h"
 #include "util/memory.h"
-#include <Windows.h>
+#include <windows.h>
 
 /* ------------------------------------------------------------------------- */
 uint32_t
 file_load_into_memory(const char* file_name, void** buffer, file_opts_e opts)
 {
-	HANDLE hFile;
-	LARGE_INTEGER buffer_size;
-	intptr_t bytes_read;
+    HANDLE hFile;
+    DWORD buffer_size;
+    DWORD bytes_read;
 
-	/* open file */
-	hFile = CreateFile(TEXT(file_name), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	if(hFile == INVALID_HANDLE_VALUE)
-	{
-		llog(LOG_ERROR, NULL, 3, "CreateFile() failed for file \"", file_name, "\"");
-		return 0;
-	}
+    /* open file */
+    hFile = CreateFile(TEXT(file_name), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if(hFile == INVALID_HANDLE_VALUE)
+    {
+        llog(LOG_ERROR, NULL, 3, "CreateFile() failed for file \"", file_name, "\"");
+        return 0;
+    }
 
-	/* get file size */
-	if(!GetFileSizeEx(hFile, &buffer_size))
-	{
-		llog(LOG_ERROR, NULL, 3, "GetFileSizeEx() failed for file \"", file_name, "\"");
-		return 0;
-	}
+    /* get file size */
+    if(!GetFileSize(hFile, &buffer_size))
+    {
+        llog(LOG_ERROR, NULL, 3, "GetFileSizeEx() failed for file \"", file_name, "\"");
+        return 0;
+    }
 
-	/* allocate buffer to copy file into */
-	if(opts & FILE_BINARY)
-		*buffer = MALLOC((intptr_t)buffer_size.QuadPart);
-	else
-		*buffer = MALLOC((intptr_t)buffer_size.QuadPart + sizeof(char));
-	if(*buffer == NULL)
+    /* allocate buffer to copy file into */
+    if(opts & FILE_BINARY)
+        *buffer = MALLOC(buffer_size);
+    else
+        *buffer = MALLOC(buffer_size + sizeof(char));
+    if(*buffer == NULL)
         OUT_OF_MEMORY("file_load_into_memory()", 0);
     
     /* copy file into buffer */
-    ReadFile(hFile, *buffer, (DWORD)buffer_size.QuadPart, &bytes_read, NULL);
-	if((intptr_t)buffer_size.QuadPart != bytes_read)
+    ReadFile(hFile, *buffer, buffer_size, &bytes_read, NULL);
+    if(buffer_size != bytes_read)
     {
         llog(LOG_ERROR, NULL, 3, "ReadFile() failed for file \"", file_name, "\"");
         return 0;
@@ -45,14 +45,14 @@ file_load_into_memory(const char* file_name, void** buffer, file_opts_e opts)
 
     /* append null terminator if not in binary mode */
     if((opts & FILE_BINARY) == 0)
-        ((char*)(*buffer))[buffer_size.QuadPart] = '\0';
+        ((char*)(*buffer))[buffer_size] = '\0';
     
-    return (uint32_t)buffer_size.QuadPart;
+    return (uint32_t)buffer_size;
 }
 
 /* ------------------------------------------------------------------------- */
 void
 free_file(void* ptr)
 {
-	FREE(ptr);
+    FREE(ptr);
 }
