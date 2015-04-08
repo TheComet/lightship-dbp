64be425dfa2675e9114ad0f1e2389669ffb44ddd
TheComet93 <alex.murray@gmx.ch>
2015-04-08 03:19:09 +0200

    Net host and net join should theoretically be working now

--- util/include/util/net.h
+++ util/include/util/net.h
@@ -21,13 +21,12 @@ net_deinit(void);
 
 /*!
  * @brief Initialises a host connection.
- * @param[in] node (optional) The address to host on. Set to NULL for auto.
  * @param[in] port The port to bind to.
  * @param[in] max_connections The maximum amount of clients allowed to connect
  * to this address.
  */
 struct net_connection_t*
-net_host_udp(const char* node, const char* port, uint32_t max_connections);
+net_host_udp(const char* port, uint32_t max_connections);
 
 /*!
  */
