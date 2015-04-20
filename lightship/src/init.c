2374715c3258d753512e4b9e71e2f8def728c36b
TheComet93 <alex.murray@gmx.ch>
2015-04-20 23:51:50 +0200

    Use yaml functions from util library instead of services

--- lightship/src/init.c
+++ lightship/src/init.c
@@ -14,8 +14,7 @@ static const char* yml_settings = "../../lightship/cfg/settings.yml";
 static const char* yml_settings = "cfg/settings.yml";
 #endif
 
-static struct plugin_t* g_plugin_yaml = NULL;
-static uint32_t g_settings_doc_id;
+static struct yaml_doc_t* g_settings_doc;
 
 typedef void (*start_loop_func)(void);
 
@@ -24,42 +23,25 @@ char
 load_core_plugins(struct game_t* game)
 {
     
-    struct plugin_info_t target;
     struct ptree_t* plugins_node;
-    
-    /*!
-     * Load the YAML plugin. This is required so the plugin manager can parse
-     * the core plugins config file and load the core plugins. If the plugin
-     * fails to load, bail.
-     */
-    target.name = "yaml";
-    target.version.major = 0;
-    target.version.minor = 0;
-    target.version.patch = 1;
-    g_plugin_yaml = plugin_load(game, &target, PLUGIN_VERSION_MINIMUM);
-    if(!g_plugin_yaml)
-        return 0;
-    if(plugin_start(game, g_plugin_yaml) == PLUGIN_FAILURE)
-    {
-        llog(LOG_FATAL, game, NULL, 1, "Failed to start YAML plugin");
-        return 0;
-    }
     
     /*
      * Try to load and start the core plugins. If that fails, bail out.
      */
-    SERVICE_CALL_NAME1(game, "yaml.load", &g_settings_doc_id, PTR(yml_settings));
-    if(!g_settings_doc_id)
+    g_settings_doc = yaml_load(yml_settings);
+    if(!g_settings_doc)
     {
         llog(LOG_WARNING, game, NULL, 3, "Config file \"", yml_settings, "\" was not found. No core plugins will be loaded");
         return 1;
     }
-    SERVICE_CALL_NAME2(game, "yaml.get_node", &plugins_node, g_settings_doc_id, "plugins");
+    
+    plugins_node = yaml_get_node(g_settings_doc, "plugins");
     if(!plugins_node)
     {
         llog(LOG_WARNING, game, NULL, 1, "Config file \"", yml_settings, "\" doesn't contain any plugins to load");
         return 1;
     }
+    
     if(!load_plugins_from_yaml_dom(game, plugins_node))
     {
         llog(LOG_FATAL, game, NULL, 1, "Couldn't start all core plugins");
@@ -105,7 +87,7 @@ init_game(char is_server)
      */
     if(!load_core_plugins(localhost))
     {
-        SERVICE_CALL_NAME1(localhost, "yaml.destroy", SERVICE_NO_RETURN, g_settings_doc_id);
+        yaml_destroy(g_settings_doc);
         game_destroy(localhost);
         localhost = NULL;
         return 0;
