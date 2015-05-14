12d7487fb29d2a145ea071da84cbc10138a2d0e8
nerdinand <nerdinand@nerdinand.com>
2015-05-14 17:38:47 +0200

    Fix test_string_death.cpp on OS X

--- tests/src/death/util/test_string_death.cpp
+++ tests/src/death/util/test_string_death.cpp
@@ -6,32 +6,38 @@
 
 using namespace testing;
 
+#ifdef LIGHTSHIP_UTIL_PLATFORM_MACOSX
+#define ASSERTION_REGEX ".*Assertion failed.*"
+#else
+#define ASSERTION_REGEX "\\bAssertion `\\w+' failed."
+#endif
+
 TEST(NAME, malloc_string_with_null_ptr)
 {
     testing::FLAGS_gtest_death_test_style = "threadsafe";
-    ASSERT_DEATH(malloc_string(NULL), "\\bAssertion `\\w+' failed.");
+    ASSERT_DEATH(malloc_string(NULL), ASSERTION_REGEX);
 }
 
 TEST(NAME, malloc_wstring_with_null_ptr)
 {
     testing::FLAGS_gtest_death_test_style = "threadsafe";
-    ASSERT_DEATH(malloc_wstring(NULL), "\\bAssertion `\\w+' failed.");
+    ASSERT_DEATH(malloc_wstring(NULL), ASSERTION_REGEX);
 }
 
 TEST(NAME, string_to_wide_string_with_null_ptr)
 {
     testing::FLAGS_gtest_death_test_style = "threadsafe";
-    ASSERT_DEATH(strtowcs(NULL), "\\bAssertion `\\w+' failed.");
+    ASSERT_DEATH(strtowcs(NULL), ASSERTION_REGEX);
 }
 
 TEST(NAME, wide_string_to_string_with_null_ptr)
 {
     testing::FLAGS_gtest_death_test_style = "threadsafe";
-    ASSERT_DEATH(wcstostr(NULL), "\\bAssertion `\\w+' failed.");
+    ASSERT_DEATH(wcstostr(NULL), ASSERTION_REGEX);
 }
 
 TEST(NAME, crlf2lf_with_null_ptr)
 {
     testing::FLAGS_gtest_death_test_style = "threadsafe";
-    ASSERT_DEATH(crlf2lf(NULL), "\\bAssertion `\\w+' failed.");
+    ASSERT_DEATH(crlf2lf(NULL), ASSERTION_REGEX);
 }
