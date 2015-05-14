25bd98445cf3605d1625770fd40df445bcc5128f
TheComet93 <alex.murray@gmx.ch>
2015-05-14 17:29:23 +0200

    Wrote unit tests for dir malloc failures

---
+++ tests/src/malloc/util/test_file_malloc.cpp
@@ -0,0 +1,16 @@
+#include "gmock/gmock.h"
+#include "util/file.h"
+#include "util/memory.h"
+
+#define NAME file_malloc
+
+using namespace testing;
+
+TEST(NAME, load_into_memory)
+{
+    void* buffer = (void*)2387; /* garbage */
+    force_malloc_fail_on();
+    ASSERT_THAT(file_load_into_memory("tests/test_dir/files/file_a.txt", &buffer, FILE_BINARY), Eq(0));
+    force_malloc_fail_off();
+    ASSERT_THAT(buffer, IsNull());
+}
--- util/src/util/platform/osx/file_osx.c
+++ util/src/util/platform/osx/file_osx.c
@@ -24,53 +24,62 @@ file_load_into_memory(const char* file_name, void** buffer, file_opts_e opts)
         return 0;
     }
 
-    /* get file descriptor and stat file */
-    fd = open(file_name, O_RDONLY);
-    if(fd == -1)
+    for(;;)
     {
-        fprintf(stderr, "open() failed for file \"%s\"\n", file_name);
-        return 0;
-    }
-    if(fstat(fd, &stbuf) != 0)
-    {
-        fprintf(stderr, "fstat() failed for file \"%s\"\n", file_name);
-        return 0;
+        /* get file descriptor and stat file */
+        fd = open(file_name, O_RDONLY);
+        if(fd == -1)
+        {
+            fprintf(stderr, "open() failed for file \"%s\"\n", file_name);
+            break;
+        }
+        if(fstat(fd, &stbuf) != 0)
+        {
+            fprintf(stderr, "fstat() failed for file \"%s\"\n", file_name);
+            break;
+        }
+
+        /* ensure file is regular file, as stbuf.st_size is only valid for regular files */
+        if(!S_ISREG(stbuf.st_mode))
+        {
+            fprintf(stderr, "File \"%s\" is not a regular file\n", file_name);
+            break;
+        }
+
+        /* compute file size */
+        buffer_size = (uintptr_t)stbuf.st_size;
+
+        /* allocate buffer to copy into */
+        if(opts & FILE_BINARY)
+            *buffer = MALLOC(buffer_size);
+        else
+            *buffer = MALLOC(buffer_size + sizeof(char));
+        if(*buffer == NULL)
+        {
+            fprintf(stderr, "malloc() failed in file_load_into_memory() -- not enough memory");
+            break;
+        }
+
+        /* copy file into buffer */
+        if(fread(*buffer, 1, buffer_size, fp) != buffer_size)
+        {
+            fprintf(stderr, "fread() failed for file \"%s\"\n", file_name);
+            break;
+        }
+
+
+        fclose(fp);
+
+        /* append null terminator if not in binary mode */
+        if((opts & FILE_BINARY) == 0)
+            ((char*)(*buffer))[buffer_size] = '\0';
+
+        return (uint32_t)buffer_size;
     }
 
-    /* ensure file is regular file, as stbuf.st_size is only valid for regular files */
-    if(!S_ISREG(stbuf.st_mode))
-    {
-        fprintf(stderr, "File \"%s\" is not a regular file\n", file_name);
-        return 0;
-    }
-
-    /* compute file size */
-    buffer_size = (uintptr_t)stbuf.st_size;
-
-    /* allocate buffer to copy into */
-    if(opts & FILE_BINARY)
-        *buffer = MALLOC(buffer_size);
-    else
-        *buffer = MALLOC(buffer_size + sizeof(char));
-    if(*buffer == NULL)
-    {
-        fprintf(stderr, "malloc() failed in file_load_into_memory() -- not enough memory");
-        return 0;
-    }
-
-    /* copy file into buffer */
-    if(fread(*buffer, 1, buffer_size, fp) != buffer_size)
-    {
-        fprintf(stderr, "fread() failed for file \"%s\"\n", file_name);
-        return 0;
-    }
     fclose(fp);
+    return 0;
 
-    /* append null terminator if not in binary mode */
-    if((opts & FILE_BINARY) == 0)
-        ((char*)(*buffer))[buffer_size] = '\0';
-
-    return (uint32_t)buffer_size;
 }
 
 /* ------------------------------------------------------------------------- */
