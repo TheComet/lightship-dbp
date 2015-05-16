d7457bee2241f1f959921dccd811f587a0b376ce
TheComet93 <alex.murray@gmx.ch>
2015-05-17 01:03:41 +0200

    Test service create

--- tests/src/misc/framework/test_services_context.cpp
+++ tests/src/misc/framework/test_services_context.cpp
@@ -27,7 +27,7 @@ public:
     struct plugin_t* plugin;
 };
 
-int callback1(int a, double b, char* str)
+int callback1(unsigned int a, double b, char* str)
 {
     return a + b + strlen(str);
 }
@@ -36,5 +36,24 @@ TEST_F(NAME, create_verify_type_info)
 {
     struct service_t* s;
     SERVICE_CREATE3(plugin, s, "test.service", (service_func)callback1, int, int, double, char*);
+
+    ASSERT_THAT(s, NotNull());
+
+    // general structure
+    EXPECT_THAT(s->directory, StrEq("test.service"));
+    EXPECT_THAT(s->exec, Eq((service_func)callback1));
+    EXPECT_THAT(s->game, Eq(game));
+
+    // check type info
+    EXPECT_THAT(s->type_info.argc, Eq(3));
+    EXPECT_THAT(s->type_info.argv_type[0], Eq(SERVICE_TYPE_UINT32));
+    EXPECT_THAT(s->type_info.argv_type[1], Eq(SERVICE_TYPE_DOUBLE));
+    EXPECT_THAT(s->type_info.argv_type[2], Eq(SERVICE_TYPE_STRING));
+    EXPECT_THAT(s->type_info.argv_type_str[0], StrEq("unsigned int"));
+    EXPECT_THAT(s->type_info.argv_type_str[1], StrEq("double"));
+    EXPECT_THAT(s->type_info.argv_type_str[2], StrEq("char*"));
+    EXPECT_THAT(s->type_info.ret_type, Eq(SERVICE_TYPE_INT32));
+    EXPECT_THAT(s->type_info.ret_type_str, StrEq("int"));
+
     service_destroy(s);
 }
