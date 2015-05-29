5aea0e3d5cbcd38310045e53b9d2e0bde462b431
TheComet93 <alex.murray@gmx.ch>
2015-05-29 23:14:23 +0200

    Actually cross compile for windows

--- scripts/test-w64-ubuntu-x86_64-pc-linux-gnu-gameserver.sh
+++ scripts/test-w64-ubuntu-x86_64-pc-linux-gnu-gameserver.sh
@@ -5,13 +5,15 @@ CROOT=x86_64-w64-mingw32
 TEST_DIR="cross-build/build/test-$CROOT/bin"
 
 ./scripts/cross-compile.py \
+    --platform Windows \
     --set-version $VERSION \
     --output-name "test-$CROOT" \
     --compiler-root /usr/bin/$CROOT \
+    --cmake "ENABLE_WINDOWS_EX=OFF" \
     --cmake "BUILD_TESTS=ON" \
     --make "make -j4" || exit 1;
 
-(cd $TEST_DIR && exec ./lightship_tests) || exit 1;
+(cd $TEST_DIR && wine lightship_tests.exe) || exit 1;
     
 
 echo "================================================"
