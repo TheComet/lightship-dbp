51b7656e51dab3d617e4d351328304b008f52b4a
TheComet93 <alex.murray@gmx.ch>
2015-08-02 13:34:38 +0200

    Unit tests for events done

--- tests/src/misc/framework/test_events.cpp
+++ tests/src/misc/framework/test_events.cpp
@@ -29,6 +29,16 @@ public:
     struct plugin_t* plugin;
 };
 
+struct ListenerInterface
+{
+	virtual void notify() = 0;
+};
+
+struct MockListener : public ListenerInterface
+{
+	MOCK_METHOD0(notify, void());
+};
+
 /*
  * Test listener for testing.
  * Arguments are (int32_t, float, const char*).
@@ -38,18 +48,42 @@ public:
  */
 EVENT_LISTENER(test_listener)
 {
+	EXTRACT_ARGUMENT_PTR(0, mock_listener, MockListener*);
 	EXTRACT_ARGUMENT(0, a, int32_t, int32_t);
 	EXTRACT_ARGUMENT(1, b, float, float);
 	EXTRACT_ARGUMENT_PTR(2, c, const char*);
+
+	mock_listener->notify();
 
 	EXPECT_THAT(a, Eq((int32_t)b));
 	EXPECT_THAT(c, StrEq("test argument"));
 }
 
+EVENT_LISTENER(test_listener_notify1)
+{
+	EXTRACT_ARGUMENT_PTR(0, mock_listener, MockListener*);
+
+	mock_listener->notify();
+}
+
+EVENT_LISTENER(test_listener_notify2)
+{
+	EXTRACT_ARGUMENT_PTR(0, mock_listener, MockListener*);
+
+	mock_listener->notify();
+}
+
+EVENT_LISTENER(test_listener_notify3)
+{
+	EXTRACT_ARGUMENT_PTR(0, mock_listener, MockListener*);
+
+	mock_listener->notify();
+}
+
 TEST_F(NAME, create_event_inits_correctly)
 {
 	struct event_t* event;
-    EVENT_CREATE3(plugin, event, "test.event", int32_t, float, const char*);
+    EVENT_CREATE4(plugin, event, "test.event", MockListener*, int32_t, float, const char*);
 
 	ASSERT_THAT(event, NotNull());
     EXPECT_THAT(event->directory, StrEq("test.event"));
@@ -59,18 +93,19 @@ TEST_F(NAME, create_event_inits_correctly)
     EXPECT_THAT(event->listeners.data, IsNull());
 
 	ASSERT_THAT(event->type_info, NotNull());
-	EXPECT_THAT(event->type_info->argc, Eq(3));
-	EXPECT_THAT(event->type_info->argv_type[0], Eq(TYPE_INT32));
-	EXPECT_THAT(event->type_info->argv_type[1], Eq(TYPE_FLOAT));
-	EXPECT_THAT(event->type_info->argv_type[2], Eq(TYPE_STRING));
-	EXPECT_THAT(event->type_info->has_unknown_types, Eq(0));
+	EXPECT_THAT(event->type_info->argc, Eq(4));
+	EXPECT_THAT(event->type_info->argv_type[0], Eq(TYPE_UNKNOWN));
+	EXPECT_THAT(event->type_info->argv_type[1], Eq(TYPE_INT32));
+	EXPECT_THAT(event->type_info->argv_type[2], Eq(TYPE_FLOAT));
+	EXPECT_THAT(event->type_info->argv_type[3], Eq(TYPE_STRING));
+	EXPECT_THAT(event->type_info->has_unknown_types, Eq(1));
 	EXPECT_THAT(event->type_info->ret_type, Eq(TYPE_VOID));
 }
 
 TEST_F(NAME, create_and_destroy_event)
 {
 	struct event_t* event;
-	EVENT_CREATE3(plugin, event, "test.event1", uint32_t, float, const char*);
+	EVENT_CREATE4(plugin, event, "test.event1", MockListener*, uint32_t, float, const char*);
 	EVENT_CREATE0(plugin, event, "test.event2");
 
 	struct event_t* event1 = event_get(game, "test.event1");
@@ -106,9 +141,48 @@ TEST_F(NAME, event_get)
 TEST_F(NAME, event_register_and_unregister_listeners)
 {
 	struct event_t* event;
-	EVENT_CREATE3(plugin, event, "test.event", int, float, const char*);
+	EVENT_CREATE4(plugin, event, "test.event", MockListener*, int, float, const char*);
 
 	EXPECT_THAT(event_register_listener(game, "test.not.an.event", test_listener), Eq(0));
 	EXPECT_THAT(event_register_listener(game, "test.event", test_listener), Ne(0));
 	EXPECT_THAT(event_register_listener(game, "test.event", test_listener), Eq(0));
+}
+
+TEST_F(NAME, fire_events)
+{
+	NiceMock<MockListener> mock;
+
+	struct event_t* event;
+	EVENT_CREATE1(plugin, event, "test.event", MockListener*);
+
+	EXPECT_THAT(event_register_listener(game, "test.event", test_listener_notify1), Ne(0));
+	EXPECT_THAT(event_register_listener(game, "test.event", test_listener_notify2), Ne(0));
+	EXPECT_THAT(event_register_listener(game, "test.event", test_listener_notify3), Ne(0));
+
+	EXPECT_CALL(mock, notify()).Times(3);
+
+	EVENT_FIRE1(event, PTR(&mock));
+
+	event_destroy(event); /* required so the event is deleted before the mock object */
+}
+
+
+TEST_F(NAME, unregister_all_listeners)
+{
+	NiceMock<MockListener> mock;
+
+	struct event_t* event;
+	EVENT_CREATE1(plugin, event, "test.event", MockListener*);
+
+	EXPECT_THAT(event_register_listener(game, "test.event", test_listener_notify1), Ne(0));
+	EXPECT_THAT(event_register_listener(game, "test.event", test_listener_notify2), Ne(0));
+	EXPECT_THAT(event_register_listener(game, "test.event", test_listener_notify3), Ne(0));
+
+	EXPECT_CALL(mock, notify()).Times(0);
+
+	event_unregister_all_listeners(event);
+
+	EVENT_FIRE1(event, PTR(&mock));
+
+	event_destroy(event); /* required so the event is deleted before the mock object */
 }
